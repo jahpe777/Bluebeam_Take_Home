9d694a7f09614d6b0dbfa8cd658d895d84df2919
James Higgs <james.y.higgs@gmail.com>
2020-05-16 18:46:49 -0700

    200516_2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,40 @@
+//run upgrade
+function upgradeDisplay() {
+  let upgrade = document.getElementById('upgrade');
+  let initial = document.getElementById('initial');
+  if (upgrade.style.display === 'none') {
+    upgrade.style.display = 'block';
+    initial.style.display = 'none';
+  } else {
+    upgrade.style.display = 'none';
+  }
+}
+
+//run not upgradeable
+function notUpgradeableDisplay() {
+  let notUpgradeable = document.getElementById('noUpgrade');
+  let initial = document.getElementById('initial');
+  if (notUpgradeable.style.display === 'none') {
+    notUpgradeable.style.display = 'block';
+    initial.style.display = 'none';
+  } else {
+    notUpgradeable.style.display = 'none';
+  }
+}
+
+//run not found
+function notFoundDisplay() {
+  let notFound = document.getElementById('noLicense');
+  let initial = document.getElementById('initial');
+  if (notFound.style.display === 'none') {
+    notFound.style.display = 'block';
+    initial.style.display = 'none';
+  } else {
+    notFound.style.display = 'none';
+  }
+}
+
+// verifying upgrade
 function login() {
   fetch(`http://www.mocky.io/v2/5dea8af93000001d442b09cd`, {
     method: 'get',
@@ -22,43 +59,16 @@ function login() {
       });
       if (result.length && result[0].isUpgradeable === true) {
         console.log('success');
+        upgradeDisplay();
       } else if (result.length && result[0].isUpgradeable === false) {
         console.log('not upgradeable');
+        notUpgradeableDisplay();
       } else {
         console.log('not found');
+        notFoundDisplay();
       }
     });
 }
 
 //run submit via click
 document.getElementById('initialSubmit').addEventListener('click', login);
-
-//run upgrade
-function upgradeDisplay() {
-  let upgrade = document.getElementById('#upgrade');
-  if (upgrade.style.display === 'none') {
-    upgrade.style.display = 'block';
-  } else {
-    upgrade.style.display = 'none';
-  }
-}
-
-//run not
-function notUpgradeableDisplay() {
-  let notUpgradeable = document.getElementById('#noUpgrade');
-  if (notUpgradeable.style.display === 'none') {
-    notUpgradeable.style.display = 'block';
-  } else {
-    notUpgradeable.style.display = 'none';
-  }
-}
-
-//run failed
-function notFoundDisplay() {
-  let notFound = document.getElementById('#noLicense');
-  if (notFound.style.display === 'none') {
-    notFound.style.display = 'block';
-  } else {
-    notFound.style.display = 'none';
-  }
-}
